1770c2b2c2947704e0b5e81ec0ad8128b5bc4dec
Steven Yesandy Candelario Fernández <steven_candelario@ucne.edu.do>
2024-09-18 19:56:25 -0400

    Implementando Hilt

--- app/src/main/java/edu/ucne/stevencandelario_p1_ap2/MainActivity.kt
+++ app/src/main/java/edu/ucne/stevencandelario_p1_ap2/MainActivity.kt
@@ -11,8 +11,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.stevencandelario_p1_ap2.ui.theme.StevenCandelario_P1_Ap2Theme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/edu/ucne/stevencandelario_p1_ap2/StevenCandelario_P1_Ap2App.kt
@@ -0,0 +1,9 @@
+package edu.ucne.stevencandelario_p1_ap2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class StevenCandelario_P1_Ap2App : Application() {
+
+}
