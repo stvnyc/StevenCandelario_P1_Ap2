402607c5ce7254357a3d47ae8cccaf81fe8dad93
Steven Yesandy Candelario Fernández <steven_candelario@ucne.edu.do>
2024-09-18 21:18:59 -0400

    Agrega el AppModule

---
+++ app/src/main/java/edu/ucne/stevencandelario_p1_ap2/di/AppModule.kt
@@ -0,0 +1,28 @@
+package edu.ucne.stevencandelario_p1_ap2.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.stevencandelario_p1_ap2.data.local.database.PrimerParcialDb
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule{
+    @Provides
+    @Singleton
+    fun providePrimerParcialDb(@ApplicationContext appContext: Context) =
+        Room.databaseBuilder(
+            appContext,
+            PrimerParcialDb::class.java,
+            "PrimerParcialDb"
+        ).fallbackToDestructiveMigration().build()
+
+    @Singleton
+    @Provides
+    fun provideAlgoDao(primerParcialDb: PrimerParcialDb) = primerParcialDb.algoDao()
+}
