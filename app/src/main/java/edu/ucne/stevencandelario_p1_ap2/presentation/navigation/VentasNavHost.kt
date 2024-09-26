d6e10c4691411cf1ceaa0bf052f2da719edad8d1
Steven Yesandy Candelario Fernández <steven_candelario@ucne.edu.do>
2024-09-25 20:53:49 -0400

    Renombrando y adaptando NavHost

---
+++ app/src/main/java/edu/ucne/stevencandelario_p1_ap2/presentation/navigation/VentasNavHost.kt
@@ -0,0 +1,32 @@
+package edu.ucne.stevencandelario_p1_ap2.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
+import edu.ucne.stevencandelario_p1_ap2.presentation.navigation.ventas.VentasListScreen
+import edu.ucne.stevencandelario_p1_ap2.presentation.navigation.ventas.VentasRegistroScreen
+
+@Composable
+fun VentasNavHost(
+    navHostController: NavHostController
+) {
+    NavHost(navController = navHostController, startDestination = Screen.VentasListScreen) {
+        composable<Screen.VentasListScreen> {
+            VentasListScreen(
+                createVenta = { navHostController.navigate(Screen.VentasRegistroScreen(0)) },
+                goToVentasRegistroScreen = { navHostController.navigate(Screen.VentasRegistroScreen(it)) }
+            )
+        }
+
+        composable<Screen.VentasRegistroScreen> {
+            val algoId = it.toRoute<Screen.VentasRegistroScreen>().Id
+            VentasRegistroScreen(
+                onGoToVentasListScreen = {navHostController.navigateUp() },
+                goBack = { navHostController.navigateUp() },
+                ventasId = algoId
+            )
+        }
+    }
+}
